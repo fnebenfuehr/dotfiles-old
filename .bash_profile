b466669384f89facb7cb0833f32704f2d18cbd08
Florian <florian.nebenfuehr@gmail.com>
2014-02-15 13:03:13 -0500

    modified paulirish's dotfiles added some stuff from mathiasbynens

--- .bash_profile
+++ .bash_profile
@@ -1,3 +1,6 @@
+# Add `~/bin` to the `$PATH`
+export PATH="$HOME/bin:$PATH"
+
 # Load ~/.extra, ~/.bash_prompt, ~/.exports, ~/.aliases and ~/.functions
 # ~/.extra can be used for settings you don’t want to commit
 for file in ~/.{extra,bash_prompt,exports,aliases,functions}; do
@@ -9,14 +12,24 @@ unset file
 . ~/code/z/z.sh
 
 # init rvm
-source ~/.rvm/scripts/rvm
+# source ~/.rvm/scripts/rvm
 
 # Case-insensitive globbing (used in pathname expansion)
 shopt -s nocaseglob
 
+# Autocorrect typos in path names when using `cd`
+shopt -s cdspell
+
 # Prefer US English and use UTF-8
 export LC_ALL="en_US.UTF-8"
 export LANG="en_US"
+
+# Enable some Bash 4 features when possible:
+# * `autocd`, e.g. `**/qux` will enter `./foo/bar/baz/qux`
+# * Recursive globbing, e.g. `echo **/*.txt`
+for option in autocd globstar; do
+	shopt -s "$option" 2> /dev/null
+done
 
 # Add tab completion for SSH hostnames based on ~/.ssh/config, ignoring wildcards
 [ -e "$HOME/.ssh/config" ] && complete -o "default" -o "nospace" -W "$(grep "^Host" ~/.ssh/config | grep -v "[?*]" | cut -d " " -f2)" scp sftp ssh
@@ -24,3 +37,6 @@ export LANG="en_US"
 # Add tab completion for `defaults read|write NSGlobalDomain`
 # You could just use `-g` instead, but I like being explicit
 complete -W "NSGlobalDomain" defaults
+
+# Bash completion for NVM
+[[ -r $NVM_DIR/bash_completion ]] && . $NVM_DIR/bash_completion
--- install-deps.sh
+++ install-deps.sh
@@ -7,12 +7,16 @@ bash < <( curl https://raw.github.com/jamiew/git-friendly/master/install.sh)
 
 # https://rvm.io
 # rvm for the rubiess
-curl -L https://get.rvm.io | bash -s stable --ruby
+# curl -L https://get.rvm.io | bash -s stable --ruby
+
+# install node version manager
+# The script clones the nvm repository to ~/.nvm and adds the source line to your profile (~/.bash_profile or ~/.profile)
+curl https://raw.github.com/creationix/nvm/master/install.sh | sh
 
 # https://github.com/isaacs/nave
 # needs npm, obviously.
 # TODO: I think i'd rather curl down the nave.sh, symlink it into /bin and use that for initial node install.
-npm install -g nave
+# npm install -g nave
 
 
 # homebrew!
@@ -23,24 +27,13 @@ ruby <(curl -fsSkL raw.github.com/mxcl/homebrew/go)
 
 # https://github.com/rupa/z
 # z, oh how i love you
+mkdir ~/code
 cd ~/code
 git clone https://github.com/rupa/z.git
 chmod +x ~/code/z/z.sh
 # also consider moving over your current .z file if possible. it's painful to rebuild :)
 
 # z binary is already referenced from .bash_profile
-
-
-# https://github.com/dronir/SpotifyControl
-# Spotify Controll Script
-cd ~/code
-git clone git://github.com/dronir/SpotifyControl.git
-
-# https://github.com/jeroenbegyn/VLCControl
-# VLC Controll Script
-cd ~/code
-git clone git://github.com/jeroenbegyn/VLCControl.git
-
 
 # my magic photobooth symlink -> dropbox. I love it.
 # first move Photo Booth folder out of Pictures
